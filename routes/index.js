72aa4c86c770d45364586862319c7db40381af9c
suhail <suhail609@gmail.com>
2021-02-13 12:24:11 +0530

    forcing user login

--- routes/index.js
+++ routes/index.js
@@ -72,7 +72,7 @@ router.get('/shopping-cart', function(req, res, next){
   res.render('shop/shopping-cart', {products: cart.generateArray(), totalPrice: cart.totalPrice});
 });
 
-router.get('/checkout', function(req, res, next){
+router.get('/checkout',isLoggedIn, function(req, res, next){
   if (!req.session.cart) {
     return res.redirect('/shopping-cart');
   }
@@ -81,7 +81,7 @@ router.get('/checkout', function(req, res, next){
   res.render('shop/checkout', {total: cart.totalPrice, errMsg: errMsg, noError: !errMsg});
 });
 
-router.post('/checkout', function(req, res, next) {
+router.post('/checkout',isLoggedIn, function(req, res, next) {
 
   if (!req.session.cart) {
     return res.redirect('/shopping-cart');
@@ -118,3 +118,11 @@ router.post('/checkout', function(req, res, next) {
 });
 
 module.exports = router;
+
+function isLoggedIn(req, res, next) {
+  if( req.isAuthenticated()) {
+    return next();
+  }
+  req.session.oldUrl = req.url;
+  res.redirect('/user/signin');
+}
